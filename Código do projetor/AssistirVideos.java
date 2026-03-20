aa56fffd2882924f308908eec0f2e84f847ce3dc
Lourivaldo wisniewski <lourivaldows01@gmail.com>
2026-03-20 16:22:21 -0300

    arrumando

--- Código do projetor/AssistirVideos.java
+++ Código do projetor/AssistirVideos.java
@@ -7,8 +7,8 @@ public class AssistirVideos {
         v[1] = new Video("Progrmando com poo");
 
         Espectador e[] = new Espectador[2];
-        e[0] = new Espectador("Louro", 22, "M", "Louro@gmail");
-        e[1] = new Espectador("Giseli", 23, "F", "Giseli@gmail");
+        e[0] = new Espectador("Louro", 22, "M", "Louro");
+        e[1] = new Espectador("Giseli", 23, "F", "Giseli");
 
         Visualizacao vis = new Visualizacao(e[0], v[0]);
         vis.avaliar(90.5f);
