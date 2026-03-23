18e3ad9e14c92786ac30b7259007ff952b2c1403
Lourivaldo wisniewski <lourivaldows01@gmail.com>
2026-03-23 15:46:37 -0300

    Update AssistirVideos.java

--- Código do projetor/AssistirVideos.java
+++ Código do projetor/AssistirVideos.java
@@ -3,12 +3,12 @@ package ProjetoGeral;
 public class AssistirVideos {
     public void main(String[] args){
         Video v[] = new Video[2];
-        v[0] = new Video("Primeiro passos em poo");
+        v[0] = new Video("Primeiro passos em POO");
         v[1] = new Video("Progrmando com poo");
 
         Espectador e[] = new Espectador[2];
-        e[0] = new Espectador("Louro", 22, "M", "Louro");
-        e[1] = new Espectador("Giseli", 23, "F", "Giseli");
+        e[0] = new Espectador("Louro", 32, "M", "Louro");
+        e[1] = new Espectador("Giseli", 25, "F", "Giseli");
 
         Visualizacao vis = new Visualizacao(e[0], v[0]);
         vis.avaliar(90.5f);
